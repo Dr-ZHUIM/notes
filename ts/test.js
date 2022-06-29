525778049f0d3a64a7d97b0cd1a914ab3da055b2
37291 <372919916@qq.com>
2022-06-29 20:19:22 +0800

    test

--- ts/test.js
+++ ts/test.js
@@ -1,2 +1,16 @@
 "use strict";
-var li = document.querySelector(".test");
+var Person = /** @class */ (function () {
+    function Person(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    ;
+    Person.prototype.showPerson = function () {
+        return this.name + "\u7684\u5E74\u9F84\u662F" + this.age + "\u5C81";
+    };
+    return Person;
+}());
+var Tom = new Person("Tom", 22);
+var Bom = new Person("Bom", 24);
+var Jack = new Person("Jack", 26);
+var persons = [Tom, Bom, Jack];
--- ts/test.ts
+++ ts/test.ts
@@ -1,1 +1,17 @@
-const li: HTMLDivElement = document.querySelector(".test") as HTMLDivElement;
+class Person{
+    constructor(name:string,age:number) {
+        this.name = name;
+        this.age = age
+    };
+    name: string;
+    age: number;
+    showPerson() {
+        return `${this.name}的年龄是${this.age}岁`
+    }
+}
+
+const Tom = new Person("Tom", 22);
+const Bom = new Person("Bom", 24);
+const Jack = new Person("Jack", 26);
+
+const persons:Person[] = [Tom,Bom,Jack] 

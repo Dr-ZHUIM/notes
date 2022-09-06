5ca364bc3e1893414c9eb87150989112f1740bfe
Dr-ZHUIM <372919916@qq.com>
2022-09-06 16:41:39 +0800

    next

--- next/nextjs-blog/components/layout.jsx
+++ next/nextjs-blog/components/layout.jsx
@@ -1,22 +1,21 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from './layout.module.scss';
-import { useState } from 'react';
+import Head from "next/head";
+import Image from "next/image";
+import styles from "./layout.module.scss";
+import { useState } from "react";
 
-const name = 'Your Name';
-export const siteTitle = 'Le Blog __ François';
+const name = "Your Name";
+export const siteTitle = "Le Blog __ François";
 
 export default function Layout({ children, home }) {
-
-  const [colorMode,setColorMode] = useState("light");
+  const [colorMode, setColorMode] = useState("light");
 
   const changeColorMode = () => {
-    setColorMode(currentColor=>{
+    setColorMode((currentColor) => {
       console.log(currentColor);
       let nextColor = currentColor === "dark" ? "light" : "dark";
-      return nextColor
-    })
-  }
+      return nextColor;
+    });
+  };
 
   return (
     <div color-mode={colorMode} className={styles.container}>
@@ -29,7 +28,7 @@ export default function Layout({ children, home }) {
         <meta
           property="og:image"
           content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle,
+            siteTitle
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
         <meta name="og:title" content={siteTitle} />
@@ -37,11 +36,12 @@ export default function Layout({ children, home }) {
       </Head>
       <section className={styles.navbarBox}>
         <Image
-         onClick={changeColorMode} 
-         src="/images/colorMode.png" 
-         alt='colorMode_img' 
-         width={24} 
-         height={24}></Image>
+          onClick={changeColorMode}
+          src="/images/colorMode.png"
+          alt="colorMode_img"
+          width={24}
+          height={24}
+        ></Image>
       </section>
       <main className={styles.mainBox}>{children}</main>
       {/* author-block */}
@@ -49,16 +49,20 @@ export default function Layout({ children, home }) {
         <div className={styles.avatorTop}>
           <Image
             src="/images/avator.jpg"
-            alt='avator'
+            alt="avator"
             width={180}
             height={180}
             className={styles.avatorTopImg}
           ></Image>
         </div>
-        <p>Hi! My name is Zhuim , a front-end engineer. Welcome to my Blog . Now I am reseraching Vite + React ecosystem . Let's learn and discuss together ! </p>
-        <p>
-          What you believe may create a elegant star night. 
-        </p>
+        <div className={styles.avatorContent}>
+          <p>
+            Hi! My name is Zhuim , a front-end engineer. Welcome to my Blog .
+            Now I am reseraching Vite + React ecosystem . Let's learn and
+            discuss together !
+          </p>
+          <p>What you believe may create a elegant star night.</p>
+        </div>
       </main>
     </div>
   );

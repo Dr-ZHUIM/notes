0693d443e916170e14def42bb49601bb7ddd02d8
Dr-ZHUIM <372919916@qq.com>
2022-10-21 18:03:08 +0800

    blogs_classify

--- next/nextjs-blog/pages/blog.jsx
+++ next/nextjs-blog/pages/blog.jsx
@@ -1,12 +1,28 @@
 import Link from "next/link";
 import { getSortedBlogsData } from "../lib/post";
 import styles from "../styles/blog.module.scss";
+import moment from "moment";
 
 export async function getStaticProps() {
   const allBlogsData = getSortedBlogsData();
+  let blogsData = [];
+  allBlogsData.map((blog) => {
+    blog.date = moment(blog.date).format("MMM d, YYYY");
+  });
+  const years = allBlogsData.map((blog) => {
+    return blog.date.slice(-4);
+  });
+  const yearClassification = new Set([...years]);
+  yearClassification.forEach((year) => {
+    const blogs = allBlogsData.filter((blogsData) => {
+      return blogsData.date.slice(-4) === year;
+    });
+    blogsData.push({ year, blogs });
+  });
+  console.log("blogsData", blogsData);
   return {
     props: {
-      allBlogsData,
+      blogsData,
     },
   };
 }
@@ -14,16 +30,27 @@ export async function getStaticProps() {
 export default function Posts(props) {
   return (
     <>
-      {props.allBlogsData.map((blogData) => {
+      {props.blogsData.map((blogData) => {
         return (
-          <div  key={blogData.id} className={styles.blogItem}>
-            <Link href={`/blog/${blogData.id}`}>
-              <p className={styles.title}>{blogData.id}</p>
-            </Link>
-            <p className={styles.date}>{blogData.date}</p>
+          <div className={styles.year} key={blogData.year}>
+            <p className={styles.year_title}>
+              {blogData.year}
+              <hr />
+            </p>
+            {blogData.blogs.map((blog) => {
+              return (
+                <div key={blog.id} className={styles.blogItem}>
+                  <Link href={`/blog/${blog.id}`}>
+                    <p className={styles.title}>{blog.id}</p>
+                  </Link>
+                  <p className={styles.date}>{blog.date}</p>
+                </div>
+              );
+            })}
           </div>
         );
       })}
     </>
   );
 }
+

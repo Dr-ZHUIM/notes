ac874d91a29792678e21b44c8c4f37b5d8ed89cf
Dr-ZHUIM <372919916@qq.com>
2022-11-24 17:42:22 +0800

    map

--- js/array/utils.js
+++ js/array/utils.js
@@ -10,6 +10,18 @@ Array.prototype._map = function (callback,thisValue) {
   return newArray;
 }
 
+Array.prototype._map2 = function (callback,thisArg) {
+  if (!callback || !callback instanceof Function) return this;
+  const result = [];
+  const length = this.length;
+  const currentArray = this;
+  for (let i = 0; i < length; i++) {
+    result[i] = callback.call(thisArg,currentArray[i],i,currentArray)
+  };
+  return result;
+}
+
+
 // map 测试用例
 // let arr = [{name:'Tom',age:20},{name:'Jamie',age:22},{name:'Donkie',age:25}];
 // let arrThis = {

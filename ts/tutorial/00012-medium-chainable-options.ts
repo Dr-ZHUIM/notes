88e103069c8fe9315959f99842de96840886a23f
Dr-ZHUIM <372919916@qq.com>
2023-01-03 17:54:50 +0800

    medium

--- ts/tutorial/00012-medium-chainable-options.ts
+++ ts/tutorial/00012-medium-chainable-options.ts
@@ -45,7 +45,7 @@ type Expected3 = {
 
 
 // ============= Your Code Here =============
-type Chainable<T extends Record<string, any>> = {
-  option<K extends string>(key: string, value: any): any
-  get(): any
+type Chainable<T = {}> = {
+  option<K extends string, V>(key:K extends keyof T ? never : K, value:V): Chainable<Omit<T,K> & {[P in K] : V}>
+  get():T
 }
